5c687066abadb8749579c06d887cf58480d571dc
Dyasoma <ddieasoma@gmail.com>
2024-06-17 12:02:24 -0600

    Completed several TODOs, Checkerboard no longer fits to entire game window. Aspect ratio constant controls scale of checkerboard centered at window center. Currently working on fixing placing pieces.

--- checkers.py
+++ checkers.py
@@ -1,16 +1,24 @@
-import random
+# TODO
+# the pieces "belong" to the board
+
+
 import pygame, sys
 from pygame.locals import *
-
-WINDOWWIDTH = 640
-WINDOWHEIGHT = 640
-DISPLAY_SURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-BOARDSIZE = 8
-SQUARESIZE = WINDOWWIDTH / BOARDSIZE
+# Constants
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 800
+SQUARECOUNT = 8
+ASPECTRATIO = 0.85
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
+DISPLAY_SURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+SQUARESIZE = WINDOWWIDTH / SQUARECOUNT
+BOARDSIZE = WINDOWWIDTH * ASPECTRATIO
+BOARDPOSX = (WINDOWWIDTH - BOARDSIZE) / 2
+BOARDPOSY = (WINDOWHEIGHT- BOARDSIZE) / 2
+
 
 
 class Board:
@@ -19,24 +27,48 @@ class Board:
         self.width = width
 
     def create_board_surface(self):
+        """
+        create_board_surface(self):
+        creates the board surface, using the pygame Surface object class, and assigning the returned
+        surface as an instance attribute of the board object. 
+        """
         self.surface = pygame.Surface((self.length, self.width))
-        square_rect = pygame.Rect(0, 0, SQUARESIZE, SQUARESIZE)
-        for i in range(BOARDSIZE):  # ith row
-            for j in range(BOARDSIZE):  # jth column
+        square_size = self.length / 8
+        square_rect = pygame.Rect(0, 0, square_size, square_size)
+
+        for i in range(SQUARECOUNT):  # ith row
+            for j in range(SQUARECOUNT):  # jth column
                 if (i + j) % 2 == 0:
                     square_rect = pygame.draw.rect(self.surface, BLACK, square_rect)
                 else:
                     square_rect = pygame.draw.rect(self.surface, WHITE, square_rect)
-                square_rect = square_rect.move(SQUARESIZE, 0)
+                square_rect = square_rect.move(square_size, 0)
             square_rect.x = 0
-            square_rect = square_rect.move(0, SQUARESIZE)
+            square_rect = square_rect.move(0, square_size)
 
-    def create_board_object(self):
-        self.board = []
-        board_row = list(range(BOARDSIZE))
-        for i in range(BOARDSIZE):
-            self.board.append(board_row)
+    def create_board_rect(self):
+        """
+        create_board_rect(self)
+        Creates an instance attribute of object Board, using the instance attribute surface. 
+        Acts as a wrapper to the get_rect() function for objects of class Surface from pygame module
+        Must be called after create_board_surface(self), and any changes made surface attribute
+        requires another call to create_board_rect(self)
+        """
+        self.rect = self.surface.get_rect()
+        self.rect.left = BOARDPOSX
+        self.rect.top = BOARDPOSY
 
+    def create_board_struct(self):
+        """
+        create_board_struct(self):
+        Creates a Data structure whose job is ???
+        structure is used to hold ???
+    
+        """
+        self.struct = []
+        board_row = list(range(SQUARECOUNT))
+        for i in range(SQUARECOUNT):
+            self.struct.append(board_row)
 
 class Piece:
     def __init__(self, position: tuple, team_color: str):
@@ -46,10 +78,11 @@ class Piece:
             self.color = RED
         else:
             self.color = BLUE
-        self.size = WINDOWWIDTH / BOARDSIZE
+        self.size = BOARDSIZE/ SQUARECOUNT
 
-    def create_piece_surface(self):
-        self.surface = pygame.Surface((SQUARESIZE, SQUARESIZE), pygame.SRCALPHA, 32)
+    def create_piece_surface(self,  board : Board):
+        square_size = board.length / SQUARECOUNT
+        self.surface = pygame.Surface((square_size, square_size), pygame.SRCALPHA, 32)
         self.surface = self.surface.convert_alpha()
 
     def create_piece_rect(self):
@@ -63,36 +96,80 @@ class Piece:
 
         ### captured???
 
-
-def create_pieces(number, team: str) -> list[Piece]:
+def init_board(board_width : int, board_height : int) -> Board:
+    """
+    init_board(board_width, board_height) -> Board:
+    Initalizes board, taking in as parameters the board width and board height as ints
+    creates object of class Board, initializing board objects surface and struct attributes
+    creates rect instance attribute.
+    Returns an instance of the Board Class
+    """
+    board = Board(BOARDSIZE, BOARDSIZE)
+    board.create_board_surface()
+    board.create_board_struct()
+    board.create_board_rect()
+    return board
+ 
+def create_pieces(number, team: str, board : Board) -> list[Piece]:
     """
     create_pieces(number, team : str) -> list[Piece]:
-    create a list whoses elements are Piece objects.
-    parameters: number is the int representing the number of pieces in the list of pieces
-                team is the str representing the team the pieces belong to.
-    returns a list of those pieces
+    creates a list whoses elements are Piece objects.
+    parameters: number is the int representing the number of pieces to make.
+    team is the str representing the team the pieces belong to.
+    Board is the current board the pieces will be placed upon
+    returns a list of Piece Objects
     """
     pieces = []
     for i in range(number):
-        piece = Piece((0, 0), team)  # all pieces will exist at the same "position"
-        piece.create_piece_surface()
+        piece = Piece((0, 0), team)  # all pieces will begin at the same "position"
+        piece.create_piece_surface(board)
         piece.create_piece_rect()
         pieces.append(piece)
     return pieces
 
-
-def place_pieces(Board, pieces: list[Piece], team_color: str):
+def set_pieces_position(pieces: list[Piece], team_color : str):
     """
-    Places pieces onto the board surface
+    set_pieces_position(pieces: list[Piece], team_color : str)
+    sets pieces position relative to the board surface, that is if the board moves, when we draw
+    the pieces they will be drawn relative to the boards top right corner
+    updates the instance attribute "position" of the object of class
+    Piece. Takes in as parameters a list of Piece objects and the team color stored as a string.
+    Side effect: mutates instance attribute "position"
+    returns : None
     """
+    """
+    roll_over = SQUARECOUNT / 2
     size = pieces[0].size
-    i = 0
-    j = 0
-    if team_color == "Blue":
-        j = BOARDSIZE - 1
-    for piece in pieces:
-        piece.position = (i * size, j * size)
-        i += 1
+    begin_box = [0, 1]
+    if team_color == "Red":
+        begin = begin_box[1]
+        j = 0
+    else: 
+        begin = begin_box[0]
+        j = SQUARECOUNT - 1
+    
+    # iterate through all pieces
+    for index in range(len(pieces)):
+        for i in range(begin, SQUARECOUNT, 2):
+            pieces[index].position = (i * size, j * size)
+            continue
+    """
+
+
+
+def init_pieces(board : Board) -> list[list[Piece]]:
+    """
+    initializes pieces, creating a list of lists whose elements are objects of class Piece.
+
+    """
+    game_pieces = []
+    player_1_pieces = create_pieces(12, "Red", board)
+    set_pieces_position(player_1_pieces, "Red")
+    player_2_pieces = create_pieces(12, "Blue", board)
+    set_pieces_position(player_2_pieces, "Blue")
+    game_pieces.append(player_1_pieces)
+    game_pieces.append(player_2_pieces)
+    return game_pieces
 
 
 def draw_pieces(board: Board, pieces: list[Piece]):
@@ -103,74 +180,24 @@ def draw_pieces(board: Board, pieces: list[Piece]):
         board.surface.blit(piece.surface, piece.position, piece.rect)
 
 
-def main():
+def draw_elements(board : Board, pieces_1: list[Piece], pieces_2: list[Piece]):
+    DISPLAY_SURF.fill("grey")
+    draw_pieces(board, pieces_1)
+    draw_pieces(board, pieces_2)
+    DISPLAY_SURF.blit(board.surface, (BOARDPOSX, BOARDPOSY))
 
+
+def main():
     pygame.display.set_caption("Checkers")
     pygame.init()
-    checkerboard = Board(WINDOWWIDTH, WINDOWHEIGHT)
-    checkerboard.create_board_surface()
-    checkerboard.create_board_object()
+    checkerboard = init_board(400, 400)
+    game_pieces = init_pieces(checkerboard)
+    player_1_pieces, player_2_pieces = game_pieces[:]
 
-    ## create pieces
-
-    player_1_pieces = create_pieces(8, "Red")
-    place_pieces(checkerboard, player_1_pieces, "Red")
-
-    player_2_pieces = create_pieces(8, "Blue")
-    place_pieces(checkerboard, player_2_pieces, "Blue")
-
-    # draw backwords
-    draw_pieces(checkerboard, player_1_pieces)
-    draw_pieces(checkerboard, player_2_pieces)
-    DISPLAY_SURF.blit(checkerboard.surface, (0, 0))
+    # draw objects
+    draw_elements(checkerboard, player_1_pieces, player_2_pieces)  
     GAME_IS_RUNNING = True
-
-    """
-    Turn system :
-    Player 1 begins by selecting a piece 
-
-    Handle Piece selection()
-
-    Player 1 moves pieces
-
-    Handle Piece movement()
-
-    Player 1 takes piece
-
-    Handle Take Piece Movement()
-        Piece can move to spot on checkerboard
-        Piece cannot move to a spot where their teams piece is
-        Piece can move to spot where other players piece is, and in doing so
-        Is allowed to move again if next movement also allows taking a piece
-        Handle double-jump, triple-jump, and N-jump
-
-
-    Handle Removing taken piece()
-        Removed Pieces have their removed Attribute set to true.
-        And they are disappeared from the checkerboard
-        Handle Cleaning board of removed pieces()
-
-    Once player 1 has made their move
-
-    Handle End Turn()
-
-    Player 2 starts Turn
-
-    If piece reaches the end of the board, Set queen Attribute to true. 
-    Handle Queen Creation()
-        > Maybe remove old piece and have a queen object?
-        > Maybe have an attribute that affects all pieces?
-
-    
-
-
-
-    
-    
-    """
-
     while GAME_IS_RUNNING:  # main game loop
-
         # collect inputs
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -179,39 +206,12 @@ def main():
 
         # handle events
 
-        # if Move valid, piece moves
-
-        # If piece takes another piece, remove second piece, restart player 1's turn.
-
         # update
-
         pygame.display.update()
     pygame.quit()
 
 
 main()
-
-
-"""
-class Piece:
-    def __init__ (self, team, position):
-        self.team = team
-        self.position = position
-        self.captured = False
-        self.queen = False
-
-    #def move(self, position):
-        #return new_position
-
-    def take(self, Piece):
-        # I don't know lol. 
-
-
-    # a piece should be able to move
-    # a piece has a position current position on the board
-    # a piece has team
-"""
-
 """
     A game of checkers.
 
@@ -238,12 +238,29 @@ class Piece:
     we continue, I take Me's Pieces Me takes my pieces.
     Eventually I take all of Me's pieces. The game is over
 """
+"""
+    Turn system :
+    Player 1 begins by selecting a piece 
+    Handle Piece selection()
+    Player 1 moves pieces
+    Handle Piece movement()
+    Player 1 takes piece
+    Handle Take Piece Movement()
+        Piece can move to spot on checkerboard
+        Piece cannot move to a spot where their teams piece is
+        Piece can move to spot where other players piece is, and in doing so
+        Is allowed to move again if next movement also allows taking a piece
+        Handle double-jump, triple-jump, and N-jump
+    Handle Removing taken piece()
+        Removed Pieces have their removed Attribute set to true.
+        And they are disappeared from the checkerboard
+        Handle Cleaning board of removed pieces()
+    Once player 1 has made their move
+    Handle End Turn()
+    Player 2 starts Turn
+    If piece reaches the end of the board, Set queen Attribute to true. 
+    Handle Queen Creation()
+        > Maybe remove old piece and have a queen object?
+        > Maybe have an attribute that affects all pieces?
 
-
-# TODO
-# Determine how sequence of surface, draw and displaying work
-# Determine how to create a checker piece
-# As a checker piece is an object, determine a way to create a checker piece class.
-# the board is an object.
-# the pieces are objects
-# the pieces "belong" to the board
+    """
